ad91040e8458002e0ecd301f6b5153f7f234ad4a
khs6226 <khsun6226@naver.com>
2019-07-23 14:14:19 -0700

    first commit

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -10,8 +10,53 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="footer-container">
+					<div class="footer-grid">
+						<div class="contact">
+							<h3>Contact Info</h3>
+							<p>
+								<i class="fa fa-envelope"></i>
+								<a href="mailto:info@inhabitent.com">info@inhabitent.com</a>
+							</p>
+							<p>
+								<i class="fa fa-phone"></i>
+								<a href="tel:7784567891">778-456-7891</a>
+							</p>
+							<p>
+							<span>
+								<i class="fa fa-facebook-square"></i>
+							</span>
+							<span>
+								<i class="fa fa-twitter-square"></i>
+							</span>
+							<span>
+								<i class="fa fa-google-plus-square"></i>
+							</span>
+							</p>
+						</div>
+					</div>
+					<div class="footer-grid">
+						<div class="business-hours">
+							<h3>Business Hours</h3>
+							<p>
+								<span class="days">Monday-Friday: </span>
+								"9am to 5pm"
+							</p>
+							<p>
+								<span class="days">Saturday: </span>
+								"10am to 2pm"
+							</p>
+							<p>
+								<span class="days">Sunday: </span>
+								"Closed"
+							</p>
+						</div>
+					</div>
+					<div class="footer-grid">
+					</div>
+				</div>
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+					<div class="copyright">Copyright &copy 2019 Inhabitent</div>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
