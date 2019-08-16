106996f8c1480dc6b77b1ed29d22cc185cb3f16a
khs6226 <khsun6226@naver.com>
2019-08-15 20:22:20 -0700

    style header. Shrink logo size and fix nav bar to the top.

--- themes/inhabitent/header-home.php
+++ themes/inhabitent/header-home.php
@@ -21,7 +21,7 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php echo esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="header-container">
+				<div class="container">
 					<div class="site-branding">
 						<div class="logo">
 							<a href="http://localhost/inhabitent">
--- themes/inhabitent/template-parts/content-home.php
+++ themes/inhabitent/template-parts/content-home.php
@@ -12,7 +12,7 @@
 	<img src="wp-content/themes/inhabitent/images/inhabitent-logo-full.svg" alt="inhabitent full logo">
 </section>
 
-<section class='shop-stuff'>
+<section class='shop-stuff container'>
 	<h2>Shop Stuff</h2>
 	<div class='products'>
 		<?php
@@ -34,7 +34,38 @@
 	</div>
 </section>
 
-<section class='journals'>
+<section class='journals container'>
 	<h2>Inhabitent Journal</h2>
-	
+	<ul>
+		<?php
+			$blog_posts = get_posts(array(
+				'post_type' => 'post',
+				'posts_per_page' => 3,
+				'order' => 'DESC',
+			));
+			foreach ($blog_posts as $post) : setup_postdata($post); ?>
+			<li>
+				<div class='thumbnail-wrapper'>
+					<?php if (has_post_thumbnail()) :
+						the_post_thumbnail('large');
+						endif; ?>
+				</div>
+				<div class='post-info-wrapper'>
+					<span class='entry-meta'>
+						<?php red_starter_posted_on('d F Y'); ?> / 
+						<?php comments_number('0 Comments', '1 Comment', '% Comments'); ?>
+					</span>
+					<h3 class='entry-title'>
+					<a href="<?php echo esc_url(get_permalink()); ?>">
+						<?php the_title(); ?>
+					</a>
+					</h3>
+				</div>
+				<a href="<?php echo esc_url(get_permalink()); ?>">
+				Read Entry
+				</a>
+			</li>
+			<?php
+			endforeach; wp_reset_postdata(); ?>
+	</ul>
 </section>
