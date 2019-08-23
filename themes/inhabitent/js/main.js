0cb778c414286e8a38c2b0f4f29683704380165e
khs6226 <khsun6226@naver.com>
2019-08-22 19:23:20 -0700

    add show and hide function to the search-bar.

--- themes/inhabitent/js/main.js
+++ themes/inhabitent/js/main.js
@@ -1,5 +1,9 @@
 $(function() {
     $('.toggle').on('click', function() {
         $('.search-label').show();
+        $('.search-field').focus();
+    });
+    $('.search-field').blur(function() {
+        $('.search-label').hide();
     });
 });
